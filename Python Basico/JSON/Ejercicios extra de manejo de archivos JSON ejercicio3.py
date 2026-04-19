c8facc0bfee2e4339da324e280ad6a5e2cce85a4
Duberney <ospinariosduberney@gmail.com>
2026-04-19 01:04:01 -0400

    correccion de variables a ingles en ejercicio3 de JSON

--- Python Basico/JSON/Ejercicios extra de manejo de archivos JSON ejercicio3.py
+++ Python Basico/JSON/Ejercicios extra de manejo de archivos JSON ejercicio3.py
@@ -3,15 +3,15 @@ import sys
 from pathlib import Path
 
 def load_pokemon_data(file_name):
-    carpeta_del_script = Path(__file__).parent
-    ruta_completa = carpeta_del_script / file_name
+    Script_Folder = Path(__file__).parent
+    Full_Path = Script_Folder / file_name
     
         
     try:
-        with open(ruta_completa, 'r', encoding='utf-8') as f:
+        with open(Full_Path, 'r', encoding='utf-8') as f:
             return json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        print(f"Error: The file '{ruta_completa}' does not exist.")
+        print(f"Error: The file '{Full_Path}' does not exist.")
         sys.exit()
 
 def show_statistics(data):
